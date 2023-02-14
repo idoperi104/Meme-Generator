4686ef00946dc2370f9d10935e9dc575566c029b
Ido Peri <idoperi@Idos-MacBook-Pro.local>
2023-02-14 19:51:22 +0200

    version 1

--- js/services/util.service.js
+++ js/services/util.service.js
@@ -28,3 +28,8 @@ function getRandomIntInclusive(min, max) {
 function getRandomColor(){
     return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
+
+function getItemByKeyValue(items, key, value) {
+    const item = items.find(item => value === item[key])
+    return item
+}
